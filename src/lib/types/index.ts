9ff9537e803233f9a79c9459ddaf6994fc86378d
Karume-lab <danielkarume@outlook.com>
2025-10-30 06:25:56 +0300

    fix: :coffin: remove manual route typing files

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,20 +1,6 @@
 import type { ProfileTypes as P } from "@/lib/constants";
-import type { FRONTEND_URLS } from "@/lib/urls/frontend-urls";
 import { env } from "./env";
 
 export type ProfileTypes = keyof typeof P;
-
-export type URLKeys = (typeof FRONTEND_URLS)[keyof typeof FRONTEND_URLS];
-
-export type SignInResponse =
-  | {
-      success: true;
-      token: string;
-      user: { id: string; email: string };
-    }
-  | {
-      success: false;
-      error: string;
-    };
 
 export { env };
