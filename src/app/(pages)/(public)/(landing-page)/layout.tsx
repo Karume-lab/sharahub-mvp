36f7b9a9494917ef228b7c73a67d51d61eee76ac
Karume-lab <danielkarume@outlook.com>
2025-11-06 16:36:44 +0300

    feat: :construction: add skeleton layout for inside the app

--- src/app/(pages)/(public)/(landing-page)/layout.tsx
+++ src/app/(pages)/(public)/(landing-page)/layout.tsx
@@ -1,4 +1,7 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import type React from "react";
+import { auth } from "@/features/auth";
 import { LandingPageAppShellWrapper } from "@/features/landing-page";
 
 interface LandingPageLayoutProps
@@ -6,7 +9,17 @@ interface LandingPageLayoutProps
     children: React.ReactNode;
   }> {}
 
-const LandingPageLayout: React.FC<LandingPageLayoutProps> = ({ children }) => {
+const LandingPageLayout: React.FC<LandingPageLayoutProps> = async ({
+  children,
+}) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return <LandingPageAppShellWrapper>{children}</LandingPageAppShellWrapper>;
 };
 
